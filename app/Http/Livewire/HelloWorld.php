bd426b424407274dcbcbc27d6ddd08e0ff3f51bf
asgarimehdi <mehdi@Home.localdomain>
2023-04-23 12:32:35 +0330

    data binding

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,6 +6,9 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {
+    public $name='Jelly';
+    public $loud=false;
+    public $greeting=['Hello'];
     public function render()
     {
         return view('livewire.hello-world',[
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,12 @@
 <div>
-    hello {{$name}}
+    <input wire:model.lazy="name" type="text">
+    <input wire:model="loud" type="checkbox">
+    <select wire:model="greeting" multiple>
+        <option>Hello</option>
+        <option>Goodby</option>
+        <option>Adios</option>
+    </select>
+    {{implode(', ',$greeting)}} {{strtoupper($name)}}
+    @if ($loud) ! @endif
+
 </div>
